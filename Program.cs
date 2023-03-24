692c8ec94cc303cb63c97facc3a2fcc8fe765956
patrickm4 <patrickmoreno444@gmail.com>
2023-03-23 22:58:42 -0700

    main feat: console output stage, move character left

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Classes;
+
 var game = new Game();
 
 game.main();
--- classes/Game.cs
+++ classes/Game.cs
@@ -1,37 +1,52 @@
-public class game {
+namespace Classes;
+
+public class Game {
   static ConsoleKeyInfo Pressed_Key;
-  public static void main() {
+  public void main() {
+
+    Console.WriteLine("Welcome to DriveBy!");
+
     Task.Run( () => updateAndDrawTheGame() );
 
     do {
-      Pressed_Key = Console.ReadKey()
-    } while (Pressed_Key.key != ConsoleLey.Escape);
+      Pressed_Key = Console.ReadKey();
+    } while (Pressed_Key.Key != ConsoleKey.Escape);
   }
 
   public static void updateAndDrawTheGame() {
     while (Pressed_Key == null); //wait for first key press
 
-    while (Pressed_Key.key != ConsoleKey.Escape) {
+    while (Pressed_Key.Key != ConsoleKey.Escape) {
       // Update the enemies, scenery, HUD etc.
-
       // Move the character if THE_PRESSED_KEY is 'A'
       // Move the character differently if THE_PRESSED_KEY is 'S'
       // etc.
-      if (Pressed_Key.key == 'A') {
+
+      char[] row = {'-','-','^','-','-'};
+
+      if (Pressed_Key.Key == ConsoleKey.A) {
+        for (var i = 0; i < row.Length; i++) {
+            if (row[i] == '^') {
+                row[i - 1] = '^';
+                row[i] = '-';
+                break;
+            }
+        }
         // Console.WriteLine("-^---")
         // move left
       }
 
-      if (Pressed_Key.key == 'D') {
+      if (Pressed_Key.Key == ConsoleKey.D) {
         // move right
       }
-      if (Pressed_Key.key == 'W') {
+      if (Pressed_Key.Key == ConsoleKey.W) {
         //move forward
       }
-      if (Pressed_Key.key == 'S') {
+      if (Pressed_Key.Key == ConsoleKey.S) {
         // move back
       }
 
+      Console.WriteLine(row);
       // Redraw the game with the updates
     }
   }
